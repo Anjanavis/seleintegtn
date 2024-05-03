63a3699ecd7432de85018ab1a3384b1d9b344eee
AnjanaAjayan <anjanaajayan19@gmail.com>
2024-05-03 15:47:10 +0530

    commit

--- gitpjt/src/gitpjt/GithubClass.java
+++ gitpjt/src/gitpjt/GithubClass.java
@@ -4,7 +4,7 @@ public class GithubClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.p.println("hello");
+   System.out.println("hello");
 	}
 
 }
